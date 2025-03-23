f31906516d1d90cac723bad92dda4c46be0104e3
zczhao1992 <zczhao1992@163.com>
2025-03-23 13:26:56 +0800

    feat: 增加登录注册

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
-      <body className={`${monaSans.className} antialiased`}>{children}</body>
+      <body className={`${monaSans.className} antialiased pattern`}>
+        {children}
+      </body>
     </html>
   );
 }
